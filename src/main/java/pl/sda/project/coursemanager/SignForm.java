9fa439d2549a23e11583e673da25a980f0df4f56
damczu <dam.czu@interia.pl>
2019-03-23 14:41:47 +0100

    Created registration.html with simple form

---
+++ src/main/java/pl/sda/project/coursemanager/SignForm.java
@@ -0,0 +1,63 @@
+package pl.sda.project.coursemanager;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class SignForm {
+    @NotEmpty
+    private String name = " ";
+
+    @NotEmpty
+    private String lastName = " ";
+
+    @NotEmpty
+    @Size(min = 7, max = 15)
+    private String login =" ";
+
+    @NotEmpty
+    @Size (min = 7, max = 30)
+    private String password = " ";
+
+    @NotEmpty
+    private String type = "USER";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
