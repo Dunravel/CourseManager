fed986a63125a1b7731a47b1e487a8ef20ed0419
Marcin Iskra <dunravel@gmail.com>
2019-03-23 15:30:52 +0100

    Added OneToOne annotation to CourseTemplates

--- src/main/java/pl/sda/project/coursemanager/persistence/Course.java
+++ src/main/java/pl/sda/project/coursemanager/persistence/Course.java
@@ -3,11 +3,8 @@ package pl.sda.project.coursemanager.persistence;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Getter
@@ -16,9 +13,10 @@ public class Course {
     @Id
     @GeneratedValue
     private Long id;
+    @OneToOne(cascade= CascadeType.ALL)
     private CourseTemplate courseTemplate;
     private LocalDate startDate;
-    private List<Users> users;
+//    private List<Users> users;
 
     public Course() {
     }
