06ea6965a023bc9edc9481707579d313c13b46ae
Marcin Iskra <dunravel@gmail.com>
2019-03-23 14:59:15 +0100

    Added view for adding course

--- src/main/java/pl/sda/project/coursemanager/persistence/CourseTemplateRepository.java
+++ src/main/java/pl/sda/project/coursemanager/persistence/CourseTemplateRepository.java
@@ -1,6 +1,8 @@
 package pl.sda.project.coursemanager.persistence;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseTemplateRepository extends CrudRepository<CourseTemplate, Long> {
 }
--- src/main/java/pl/sda/project/coursemanager/services/AdminController.java
+++ src/main/java/pl/sda/project/coursemanager/services/AdminController.java
@@ -1,15 +1,17 @@
 package pl.sda.project.coursemanager.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.sda.project.coursemanager.persistence.CourseTemplate;
 import pl.sda.project.coursemanager.persistence.CourseTemplateRepository;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class AdminController {
 
     @Autowired
@@ -22,19 +24,57 @@ public class AdminController {
         return m;
     }
 
-    @GetMapping("/admin/listCourses")
-    private ModelAndView adminCourses(){
+//    @GetMapping("/admin/listCourses")
+//    private ModelAndView adminCourses(){
+//
+//        Iterable<CourseTemplate> courseTemplates = courseTemplateRepository.findAll();
+//
+//        ModelAndView view = new ModelAndView();
+//        view.setViewName("courseList");
+//        view.addObject("courses",courseTemplates);
+//        return view;
+//    }
+//
+//    @PostMapping("/admin/createCourse")
+//    private void createCourseTemplate(){
+//
+//    }
 
-        Iterable<CourseTemplate> courseTemplates = courseTemplateRepository.findAll();
-
-        ModelAndView view = new ModelAndView();
-        view.setViewName("courseList");
-        view.addObject("courses",courseTemplates);
-        return view;
+    @GetMapping("/admin/newCourse")
+    public String showAddForm(CourseTemplate courseTemplate){
+        System.out.println("test");
+        return "add-course";
     }
 
-    @GetMapping("/admin/createCourse")
-    private void createCourseTemplate(String courseName){
-        CourseTemplate courseTemplate = new CourseTemplate(courseName);
+    @PostMapping("/admin/addCourse")
+    public String addCourse(CourseTemplate courseTemplate, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "new-course";
+        }
+
+        courseTemplateRepository.save(courseTemplate);
+        model.addAttribute("courses",courseTemplateRepository.findAll());
+        return "admin";
+    }
+
+    @GetMapping("/admin/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model){
+        CourseTemplate courseTemplate = courseTemplateRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user ID: " + id));
+
+        model.addAttribute("course",courseTemplate);
+        return "update-course";
+    }
+
+    @PostMapping("/admin/update/{id}")
+    public String updateCourse(@PathVariable("id") Long id, CourseTemplate courseTemplate, BindingResult result, Model model){
+        if(result.hasErrors()){
+            courseTemplate.setId(id);
+            return "update-course";
+        }
+
+        courseTemplateRepository.save(courseTemplate);
+        model.addAttribute("courses", courseTemplateRepository.findAll());
+        return "admin";
     }
 }
