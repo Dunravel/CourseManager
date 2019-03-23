5ba8ffa8b7e5fbc0fdeca72de1c37e829e424577
Marcin Iskra <dunravel@gmail.com>
2019-03-23 11:02:50 +0100

    Added id field to CourseTemplate

--- src/main/java/pl/sda/project/coursemanager/persistance/CourseTemplate.java
+++ src/main/java/pl/sda/project/coursemanager/persistance/CourseTemplate.java
@@ -4,12 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Setter
 @Getter
 public class CourseTemplate {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String courseName;
+
 
     public CourseTemplate(){
 
