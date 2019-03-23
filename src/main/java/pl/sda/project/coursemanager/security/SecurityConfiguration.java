47fcf1cdc38262e26c5e43740923581386ec1a4d
Marcin Iskra <dunravel@gmail.com>
2019-03-23 12:25:16 +0100

    Added missing annotation

--- src/main/java/pl/sda/project/coursemanager/security/SecurityConfiguration.java
+++ src/main/java/pl/sda/project/coursemanager/security/SecurityConfiguration.java
@@ -1,8 +1,10 @@
 package pl.sda.project.coursemanager.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
