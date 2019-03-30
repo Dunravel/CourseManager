fa034e11a1554eb7aef5b6f0dd855d68fece5798
Marcin Iskra <dunravel@gmail.com>
2019-03-30 10:32:05 +0100

    Added list of Lessons to Block

--- src/main/java/pl/sda/project/coursemanager/persistence/Block.java
+++ src/main/java/pl/sda/project/coursemanager/persistence/Block.java
@@ -1,13 +1,19 @@
 package pl.sda.project.coursemanager.persistence;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Block {
     @Id
     @GeneratedValue
     private Long id;
     private String name;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Lesson> lessons;
 }
