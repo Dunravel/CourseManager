2b7eeae0f707dd09df4dee8f936b0d8579908210
Marcin Iskra <dunravel@gmail.com>
2019-03-30 10:23:28 +0100

    Added BlockRepository

---
+++ src/main/java/pl/sda/project/coursemanager/persistence/BlockRepository.java
@@ -0,0 +1,7 @@
+package pl.sda.project.coursemanager.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlockRepository extends CrudRepository<Block,Long> {
+
+}
