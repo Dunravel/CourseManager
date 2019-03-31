644941e8122eec3563a9a654db2b5f90f3d52be8
damczu <dam.czu@interia.pl>
2019-03-31 15:26:59 +0200

    Added Role class

---
+++ src/main/java/pl/sda/project/coursemanager/persistence/RoleRepository.java
@@ -0,0 +1,6 @@
+package pl.sda.project.coursemanager.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository <Role,Long> {
+}
